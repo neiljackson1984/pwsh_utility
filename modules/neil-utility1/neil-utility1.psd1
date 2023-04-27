4a332b252c1950f1845238a41bb746ec001aaf67
Neil Jackson <neil@rattnow.com>
2023-04-27 14:28:34 -0700

    checkpoint. experimenting with external dependencies

--- modules/neil-utility1/neil-utility1.psd1
+++ modules/neil-utility1/neil-utility1.psd1
@@ -1,5 +1,12 @@
+
+# see [https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_module_manifests?view=powershell-7.3]
+# see [https://learn.microsoft.com/en-us/powershell/scripting/developer/module/how-to-write-a-powershell-module-manifest?view=powershell-7.3]
+# see [https://stackoverflow.com/questions/55668072/how-to-cause-install-module-to-also-install-any-required-modules]
 @{
     ModuleVersion="0.0.1"
+    Author = "Neil Jackson<neil@rattnow.com>"
+    Description = "useful utilities and dependencies"
+    GUID = '1e59d151-70ae-4cdc-9df3-83f6b7703ac1'
     NestedModules=@(
         "ipv6-control.psm1"
         "Set-Owner.psm1"
@@ -13,5 +20,33 @@
         "autodesk_popup_slapdown.psm1"
         # no good way to do wildcards here, unfortunately
     )
+
+
+    RequiredModules=@(
+        
+        @{
+            # [https://github.com/christaylorcodes/ConnectWiseControlAPI]
+            ModuleName = 'ConnectWiseControlAPI'
+            GUID = 'f94fa996-0f01-4c5c-9cd9-3227728ebacb'
+            ModuleVersion = '0.3.5.0'
+        }
+
+    )
+
+
+
+    # ExternalModuleDependencies = @('ConnectWiseControlAPI')
+     PrivateData = @{
+         ExternalModuleDependencies = @('ConnectWiseControlAPI')
+         PSData = @{
+             ExternalModuleDependencies = @('ConnectWiseControlAPI')
+         } 
+     } 
+    ###  
+    ###  # damnit -- it looks like cross-repository dependencies are not well
+    ###  # supported by the Powershell package management architecture.
+    ###  #
+    ###  # I don't know what the best workaround here is -- vendoring?  seems
+    ###  # heavy-handed and ahrd to maintain.
     
 }
--- modules/neil-utility1/scratchpads/scratchpad_install.ps1
+++ modules/neil-utility1/scratchpads/scratchpad_install.ps1
@@ -101,3 +101,159 @@ powershell Install-Module -Name Microsoft.Graph
 
 import-module ./utility.psm1
 addEntryToPSModulePathPersistently "C:\work\pwsh_utility\modules"
+
+
+# 2023-04-27-1317
+#%%
+Get-InstalledModule | Sort Name
+Get-Help Install-Module -Online
+Get-Help Get-InstalledModule -Online
+Update-Help
+
+Get-PSRepository | fl
+Get-PSRepository | get-member
+@(Get-PSRepository)[0].PackageManagementProvider | fl
+@(Get-PSRepository)[0].PackageManagementProvider.GetType().FullName
+
+Get-PackageProvider | fl
+@(Get-PackageProvider)[0].GetType().FullName
+
+Find-PackageProvider
+
+#see [https://learn.microsoft.com/en-us/powershell/gallery/how-to/working-with-local-psrepositories?view=powershellget-2.x]
+#%%
+$pathOfLocalRepository = "c:\work\neil-powershell-repository1"
+$nameOfLocalRepository = (split-path $pathOfLocalRepository -Leaf)
+New-Item  $pathOfLocalRepository -ItemType 'Directory' -ErrorAction SilentlyContinue
+Unregister-PSRepository -Name $nameOfLocalRepository
+@{
+    Name                    = $nameOfLocalRepository
+    SourceLocation          = $pathOfLocalRepository
+    # ScriptSourceLocation    = $pathOfLocalRepository
+    InstallationPolicy      = 'Trusted'
+} | % {Register-PSRepository @_}
+#%%
+# Unregister-PSRepository -Name "neil-ps-repository1"
+# 
+#%%
+Get-Help -Online Register-PSRepository
+Get-Help -Online UnRegister-PSRepository
+Get-Help -Online Publish-Module
+Get-Help -Online Test-ModuleManifest
+Get-Help  Publish-Module
+
+#%%
+# @{
+#     Name = 'ConnectWiseControlAPI'
+#     Repository = 'PSGallery'
+#     AcceptLicense = $True
+#     Path = $pathOfLocalRepository
+# } | % { Save-Module @_ }
+#%%
+@{
+    Name = 'ConnectWiseControlAPI'
+    # Source = 'PSGallery'
+    Source = (Get-PackageSource PSGallery).Location
+
+    # AcceptLicense = $True
+    Path = $pathOfLocalRepository
+} | % { Save-Package @_ }
+#%%
+Get-PackageSource PSGallery | fl
+
+Get-Package 'ConnectWiseControlAPI'
+Find-Package 'ConnectWiseControlAPI' | fl
+$pathOfModuleManifestFile = "C:\work\pwsh_utility\modules\neil-utility1\neil-utility1.psd1"
+$pathOfModule = (split-path $pathOfModuleManifestFile -Parent)
+@{
+    Path = $pathOfModule
+    Repository = $nameOfLocalRepository
+} | % {Publish-Module @_}
+
+#>>>  Publish-PSArtifactUtility: PowerShellGet cannot resolve the module dependency
+#>>>  'ConnectWiseControlAPI' of the module 'neil-utility1' on the repository
+#>>>  'neil-powershell-repository1'. Verify that the dependent module
+#>>>  'ConnectWiseControlAPI' is available in the repository
+#>>>  'neil-powershell-repository1'. If this dependent module
+#>>>  'ConnectWiseControlAPI' is managed externally, add it to the
+#>>>  ExternalModuleDependencies entry in the PSData section of the module manifest.
+
+# After I added said ExternalModuleDependencies entry, the above command than
+# ran succesfully. I happened to have the ConnectWiseControlAPI module installed
+# when I ran the above command -- I am not sure how, if at all, the fact of a
+# dependency being installed affects the operation of the above command -- I
+# would expect it to have no effect.
+#
+# The result of running the above commnad is that a single file, named
+# "neil-utility1.0.0.1.nupkg" appeared in the directory $pathOfLocalRepository .
+# The nupkg file seemed to contain absolutely every file from the directory
+# $pathOfModule, along with several newly-created files containing metadata.
+#%%
+
+
+
+Test-ModuleManifest $pathOfModuleManifestFile
+explorer.exe "/root,$($pathOfLocalRepository)"
+Install-Module -confirm:$false -force 'ConnectWiseControlAPI'
+Get-InstalledModule 'ConnectWiseControlAPI' | fl
+Get-Module 'ConnectWiseControlAPI' | fl
+Import-Module 'ConnectWiseControlAPI' 
+
+Get-InstalledModule 'ConnectWiseControlAPI'
+Get-InstalledModule 'neil-utility1'
+
+UnInstall-Module 'ConnectWiseControlAPI'
+UnInstall-Module -Force 'ConnectWiseControlAPI'
+Uninstall-Module -Force  'neil-utility1'
+
+##>>>   PS U:\scripting_journal> UnInstall-Module 'ConnectWiseControlAPI'
+##>>>   Uninstall-Package: The module 'ConnectWiseControlAPI' of version '0.3.5.0' in module base folder 'C:\Users\Admin\Documents\PowerShell\Modules\ConnectWiseControlAPI\0.3.5.0' cannot be uninstalled, because one or more other modules 'neil-utility1' are dependent on this module. Uninstall the modules
+##>>>   that depend on this module before uninstalling module 'ConnectWiseControlAPI'.
+##>>>   PS U:\scripting_journal> Uninstall-Module 'neil-utility1'
+##>>>   Uninstall-Package: No match was found for the specified search criteria and module names 'neil-utility1'.
+##>>>   PS U:\scripting_journal> 
+
+UnInstall-Module 'ConnectWiseControlAPI' -Force
+UnInstall-Module 'neil-utility1' -Force
+UnInstall-Package 'neil-utility1' -Force
+
+#%%
+Install-Module -Name "neil-utility1"  -Repository "neil-powershell-repository1"
+Install-Module -Name "ConnectWiseControlAPI"  -Repository "neil-powershell-repository1"
+#%%
+Import-Module neil-utility1
+Import-Module -Force neil-utility1
+Get-Module neil-utility1 | fl
+Get-InstalledModule neil-utility1 | fl
+
+Find-Module neil-utility1
+
+Get-Package 'ConnectWiseControlAPI' | fl
+Find-Package 'ConnectWiseControlAPI' | fl
+
+Install-Package 'ConnectWiseControlAPI'
+Get-InstalledModule 'ConnectWiseControlAPI' | fl
+(Get-InstalledModule 'ConnectWiseControlAPI').Metadata
+(Find-Package 'ConnectWiseControlAPI').Metadata.GetType().FullName
+(Find-Package 'ConnectWiseControlAPI').Metadata | get-member
+(Find-Package 'ConnectWiseControlAPI') | fl
+(Find-Package 'ConnectWiseControlAPI').Metadata['PackageManagementProvider']
+
+Find-Package '*7zip*'
+Get-PackageSource
+winget --help
+
+winget source list
+
+get-packageprovider
+find-module xjea
+
+update-module PowershellGet
+Get-Module PowershellGet
+Install-Module PowerShellGet
+Get-InstalledModule PowerShellGet 
+
+Find-Module PowershellGet | fl
+Start-Process "$((Find-Module PowershellGet ).ProjectUri)"
+
+Get-Module PowershellGet | fl
