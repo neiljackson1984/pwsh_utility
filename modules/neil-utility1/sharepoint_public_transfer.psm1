b808854d7f58d18a1fbf3d25cf0a47a93ef2a1a1
Neil Jackson <neil@rattnow.com>
2024-07-27 14:10:46 -0700

    checkpoint

--- modules/neil-utility1/sharepoint_public_transfer.psm1
+++ modules/neil-utility1/sharepoint_public_transfer.psm1
@@ -2,12 +2,18 @@
 <# 2024-07-23-1241:
     It's not great to have these urls hardcoded here, but better here once than a thousand times in various scripts.
 #>
-$publicUrlOfMyPubliclyEditableSharepointFolder = "https://autoscaninc-my.sharepoint.com/:f:/p/neil/Es4XlxUoKpNPoQQAVV1RBR0B-E-toUZeyj_lcANK_4McSg"
-$urlOfMyPubliclyEditableSharepointFolder       = "https://autoscaninc-my.sharepoint.com/personal/neil_autoscaninc_com/Documents/Attachments/fa4541d00e6e4ef8b1ade357f0e207e5"
+## $publicUrlOfMyPubliclyEditableSharepointFolder = "https://autoscaninc-my.sharepoint.com/:f:/p/neil/Es4XlxUoKpNPoQQAVV1RBR0B-E-toUZeyj_lcANK_4McSg"
+## $urlOfMyPubliclyEditableSharepointFolder       = "https://autoscaninc-my.sharepoint.com/personal/neil_autoscaninc_com/Documents/Attachments/fa4541d00e6e4ef8b1ade357f0e207e5"
+<# I amn hardcoding these urls within the function bodies, which is somewhat bad
+practice, in order to make the functions self-contained in order to work with my
+naive method of copying fuinction defintions temporarily to remote computers;
+M<y naive method does not properyl handle script/module variables. #>
 
 
 function uploadFileToSpecialPublicSharepointFolder([string] $pathOfSourceFile, [string] $pathOfDestinationFile = $null){
-
+    $publicUrlOfMyPubliclyEditableSharepointFolder = "https://autoscaninc-my.sharepoint.com/:f:/p/neil/Es4XlxUoKpNPoQQAVV1RBR0B-E-toUZeyj_lcANK_4McSg"
+    $urlOfMyPubliclyEditableSharepointFolder       = "https://autoscaninc-my.sharepoint.com/personal/neil_autoscaninc_com/Documents/Attachments/fa4541d00e6e4ef8b1ade357f0e207e5"
+    
     
     # hit the $publicUrlOfMyPubliclyEditableSharepointFolder solely for the
     # purpose of collecting the cookies that we will need for the actual
@@ -40,7 +46,9 @@ function uploadFileToSpecialPublicSharepointFolder([string] $pathOfSourceFile, [
 }
 
 function downloadFileFromSpecialPublicSharepointFolder([string] $pathOfSourceFile, [string] $pathOfDestinationFile){
-    
+    $publicUrlOfMyPubliclyEditableSharepointFolder = "https://autoscaninc-my.sharepoint.com/:f:/p/neil/Es4XlxUoKpNPoQQAVV1RBR0B-E-toUZeyj_lcANK_4McSg"
+    $urlOfMyPubliclyEditableSharepointFolder       = "https://autoscaninc-my.sharepoint.com/personal/neil_autoscaninc_com/Documents/Attachments/fa4541d00e6e4ef8b1ade357f0e207e5"
+
     # hit the $publicUrlOfMyPubliclyEditableSharepointFolder solely for the
     # purpose of collecting the cookies that we will need for the actual
     # uploading:
