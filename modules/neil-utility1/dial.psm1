97acb2c65d30f1387d09208bf8c4704a19888ea2
Neil Jackson <neil@rattnow.com>
2025-10-17 14:43:26 -0700

    now memoizing polycom phone api parameters to avoid the penalty of looking them up at the time of module load.

--- modules/neil-utility1/dial.psm1
+++ modules/neil-utility1/dial.psm1
@@ -7,59 +7,59 @@ we load the module. #>
 
 $bitwardenItemIdOfNeilDeskphone = 'c63c4108-b859-45f7-b4a8-b36b0182cfa7'
 
-.{ # define $restApiCommonParametersOfSamplePhone, which we will use for testing
-    # the username for the rest api is "Polycom"
-    # this is the url of the web admin interface for our sample phone:
+$script:polycomPhoneRestApiParameters = $null
+function Get-PolycomPhoneRestApiParameters {
+    [CmdletBinding()]
+    [OutputType([HashTable])]
+    param(
 
-    $macAddressOfSamplePhone = (getFieldMapFromBitwardenItem $bitwardenItemIdOfNeilDeskphone).mac_address
-
-    ##$webAdminUrlOfSamplePhone = "https://phone$macAddressOfSamplePhone.ad.autoscaninc.com"
-    $webAdminUrlOfSamplePhone = (get-bitwardenitem $bitwardenItemIdOfNeilDeskphone).login.uris[0].uri
-    # this is the url of the web admin interface for our sample phone:
-
-
-    $restApiUsernameOfSamplePhone = "Polycom"
-
-    $restApiPasswordOfSamplePhone = (Get-BitwardenItem $bitwardenItemIdOfNeilDeskphone).login.password
-    # $restApiPasswordOfSamplePhone = $preferredLocalAdminPassword
-    # $restApiPasswordOfSamplePhone = "456"
-
-    $restApiRootUrlOfSamplePhone = "$webAdminUrlOfSamplePhone"
-
-    $candidateCredentialsForSamplePhoneRestApi = @(
-        foreach($password in @(
-                @(
-                    $restApiPasswordOfSamplePhone
-                    (Get-BitwardenItem $bitwardenItemIdOfNeilDeskphone).login.password
-                    (Get-BitwardenItem $webAdminUrlOfSamplePhone).login.password
-                    # $preferredLocalAdminPassword
-                ) | ? {$_} |
-                select -unique
-            )
-        ){
-            New-Object System.Management.Automation.PSCredential -ArgumentList @(
-                $restApiUsernameOfSamplePhone
-                (ConvertTo-SecureString $password -AsPlainText -Force)
-            )
-        }
     )
 
+    if(-not $polycomPhoneRestApiParameters){
+        write-information "constructing polycomPhoneRestApiCommonParameters"
+        $bitwardenItem = get-bitwardenitem $bitwardenItemIdOfNeilDeskphone
 
-    $restApiCommonParametersOfSamplePhone = @{
-        Credential = New-Object System.Management.Automation.PSCredential @(
-            $restApiUsernameOfSamplePhone
-            (ConvertTo-SecureString $restApiPasswordOfSamplePhone -AsPlainText -Force)
-        )
-        ContentType = "application/json"
-        SkipCertificateCheck = $True
+
+        # define $restApiCommonParameters, which we will use for testing
+        # the username for the rest api is "Polycom"
+        # this is the url of the web admin interface for our sample phone:
+
+        ## $macAddressOfPhone = $(getFieldMapFromBitwardenItem $bitwardenItem.id).mac_address
+
+        ##$webAdminUrl = "https://phone$macAddressOfPhone.ad.autoscaninc.com"
+        $webAdminUrl = $bitwardenItem.login.uris[0].uri
+        # this is the url of the web admin interface for our phone:
+
+        $restApiUsername = "Polycom"
+        $restApiPassword = $bitwardenItem.login.password
+
+        $script:polycomPhoneRestApiParameters  = @{
+            rootUrl  = "$webAdminUrl"
+            commonRequestParameters = @{
+                Credential = New-Object System.Management.Automation.PSCredential @(
+                    <# string username #>
+                    $restApiUsername
+
+                    <# secureString password #>
+                    (ConvertTo-SecureString $restApiPassword -AsPlainText -Force)
+                )
+                ContentType = "application/json"
+                SkipCertificateCheck = $True
+            }
+
+        }
     }
 
+    return $script:polycomPhoneRestApiParameters 
+}
+
+if($false){   
 
     function samplePhoneConfigSet([string] $name, [string] $value){
         $succeeded = $false
         foreach($credential in $candidateCredentialsForSamplePhoneRestApi){
             # Write-Host "trying credential"
-            $restApiParameters = $restApiCommonParametersOfSamplePhone.Clone()
+            $restApiParameters = $restApiCommonParameters.Clone()
             $restApiParameters['Credential'] = $credential
             $response = $null
             $response = @{
@@ -97,7 +97,7 @@ $bitwardenItemIdOfNeilDeskphone = 'c63c4108-b859-45f7-b4a8-b36b0182cfa7'
         $succeeded = $false
         foreach($credential in $candidateCredentialsForSamplePhoneRestApi){
             # Write-Host "trying credential"
-            $restApiParameters = $restApiCommonParametersOfSamplePhone.Clone()
+            $restApiParameters = $restApiCommonParameters.Clone()
             $restApiParameters['Credential'] = $credential
             $response = $null
             $response = @{
@@ -145,7 +145,7 @@ $bitwardenItemIdOfNeilDeskphone = 'c63c4108-b859-45f7-b4a8-b36b0182cfa7'
 
             Write-Host "trying credential"
 
-            $restApiParameters = $restApiCommonParametersOfSamplePhone.Clone()
+            $restApiParameters = $restApiCommonParameters.Clone()
             $restApiParameters['Credential'] = $credential
 
             $response = $null
@@ -181,10 +181,13 @@ function dial_deprecated1 {
         [switch] $initial = $false
     )
 
+
+    $commonRequestParameters = (Get-PolycomPhoneRestApiParameters).commonRequestParameters
+
     if($initial){
-        (Invoke-WebRequest @restApiCommonParametersOfSamplePhone -Method POST -Uri "$restApiRootUrlOfSamplePhone/api/v1/callctrl/dial" -Body (ConvertTo-JSON @{data=@{Dest=$digits}}) ).Content | write-information
+        (Invoke-WebRequest @commonRequestParameters -Method POST -Uri "$((Get-PolycomPhoneRestApiParameters).rootUrl)/api/v1/callctrl/dial" -Body (ConvertTo-JSON @{data=@{Dest=$digits}}) ).Content | write-information
     }  else {
-        (Invoke-WebRequest @restApiCommonParametersOfSamplePhone -Method POST -Uri "$restApiRootUrlOfSamplePhone/api/v1/callctrl/sendDTMF" -Body (ConvertTo-JSON @{data=@{Digits=$digits}}) ).Content | write-information
+        (Invoke-WebRequest @commonRequestParameters -Method POST -Uri "$((Get-PolycomPhoneRestApiParameters).rootUrl)/api/v1/callctrl/sendDTMF" -Body (ConvertTo-JSON @{data=@{Digits=$digits}}) ).Content | write-information
 
     }
 }
@@ -195,11 +198,13 @@ function dial {
         [string] $digits
     )
 
+    $commonRequestParameters = (Get-PolycomPhoneRestApiParameters).commonRequestParameters
+
     $state = $(
         @{
             Method="GET" 
-            Uri = "$restApiRootUrlOfSamplePhone/api/v1/mgmt/pollForStatus" 
-        } | % {Invoke-WebRequest @restApiCommonParametersOfSamplePhone @_} |
+            Uri = "$((Get-PolycomPhoneRestApiParameters).rootUrl)/api/v1/mgmt/pollForStatus" 
+        } | % {Invoke-WebRequest @commonRequestParameters @_} |
         select -expand Content |
         ConvertFrom-Json -depth 50 |
         % data | % State
@@ -208,11 +213,12 @@ function dial {
     write-information "$(get-date): state: $($state). dialing '$($digits)'"
 
     if($state -eq "Active"){
-        (Invoke-WebRequest @restApiCommonParametersOfSamplePhone -Method POST -Uri "$restApiRootUrlOfSamplePhone/api/v1/callctrl/sendDTMF" -Body (ConvertTo-JSON @{data=@{Digits=$digits}}) ).Content | write-information
+        (Invoke-WebRequest @commonRequestParameters -Method POST -Uri "$((Get-PolycomPhoneRestApiParameters).rootUrl)/api/v1/callctrl/sendDTMF" -Body (ConvertTo-JSON @{data=@{Digits=$digits}}) ).Content | write-information
 
     } else {
-        (Invoke-WebRequest @restApiCommonParametersOfSamplePhone -Method POST -Uri "$restApiRootUrlOfSamplePhone/api/v1/callctrl/dial" -Body (ConvertTo-JSON @{data=@{Dest=$digits}}) ).Content | write-information
+        (Invoke-WebRequest @commonRequestParameters -Method POST -Uri "$((Get-PolycomPhoneRestApiParameters).rootUrl)/api/v1/callctrl/dial" -Body (ConvertTo-JSON @{data=@{Dest=$digits}}) ).Content | write-information
     } 
 }
 
 Export-ModuleMember -function dial
+Export-ModuleMember -function Get-PolycomPhoneRestApiParameters
